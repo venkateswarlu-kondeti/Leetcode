8a77346efa6298e31cea9881c719f80673499e68
venchitin <94561919+venchitin@users.noreply.github.com>
2021-12-03 09:08:09 -0600

    Second commit

--- src/com/company/ReplaceGreatestRight.java
+++ src/com/company/ReplaceGreatestRight.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class ReplaceGreatestRight {
+    public int[] replaceElements(int[] arr) {
+
+    }
+
+    public static void main(String[] args)
+    {
+        ReplaceGreatestRight rgr=new ReplaceGreatestRight();
+        int[] arr={17,18,5,4,6,1};
+        System.out.println(rgr.replaceElements(arr));
+    }
 }
