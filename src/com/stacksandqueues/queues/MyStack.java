d9a7533699886b4c34532602f0b9c3c9a3df1b12
Venky K <venchitin@gmail.com>
2024-04-15 09:13:06 -0500

    First Commit 04/15/2024

--- src/com/stacksandqueues/queues/MyStack.java
+++ src/com/stacksandqueues/queues/MyStack.java
@@ -7,47 +7,49 @@ import java.util.Queue;
 
 public class MyStack {
     Queue<Integer> queue1;
-    Queue<Integer> queue2;
     List<Integer> numList;
-    public MyStack(){
+    public MyStack() {
         queue1=new LinkedList<>();
-        queue2=new LinkedList<>();
         numList=new ArrayList<>();
     }
-    public void push(int x){
-        queue2.clear();
+
+    public void push(int x) {
+        queue1.clear();
         queue1.add(x);
         numList.add(queue1.remove());
         for(int i=numList.size()-1;i>=0;i--){
-            queue2.add(numList.get(i));
+            queue1.add(numList.get(i));
         }
     }
-    public int pop(){
-        numList.remove(numList.size()-1);
-        return queue2.remove();
-    }
-    public int top(){
 
-        return queue2.isEmpty()? 0: queue2.peek();
+    public int pop() {
+        numList.remove(numList.size()-1);
+        return queue1.remove();
     }
-    public boolean empty(){
-        return queue2.isEmpty();
+
+    public int top() {
+        return queue1.isEmpty()? 0: queue1.peek();
     }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+
     public static void main(String[] args){
         MyStack myStack=new MyStack();
-        myStack.push(1);
+        /*myStack.push(1);
         myStack.push(2);
         System.out.println(myStack.top());
         System.out.println(myStack.pop());
-        System.out.println(myStack.empty());
+        System.out.println(myStack.empty());*/
 
-        /*myStack.push(1);
+        myStack.push(1);
         myStack.push(2);
         myStack.push(3);
         System.out.println(myStack.pop());
         myStack.push(4);
         System.out.println(myStack.pop());
         System.out.println(myStack.pop());
-        System.out.println(myStack.pop());*/
+        System.out.println(myStack.pop());
     }
 }
