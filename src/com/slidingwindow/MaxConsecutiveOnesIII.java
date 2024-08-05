c30e5617ba4dc1830f94d70eb0eeda9d9ec8f0b7
venkateswarlu-kondeti <venchitin@gmail.com>
2024-08-05 17:29:56 -0500

    Today First Commit 08/05/2024

--- src/com/slidingwindow/MaxConsecutiveOnesIII.java
+++ src/com/slidingwindow/MaxConsecutiveOnesIII.java
@@ -3,6 +3,24 @@ package com.slidingwindow;
 public class MaxConsecutiveOnesIII {
     public int longestOnes(int[] nums, int k){
         int left=0;
+        int ans=0;
+        int curr=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]==0){
+                curr++;
+            }
+            while(curr>k){
+                if(nums[left]==0){
+                    curr--;
+                }
+                left++;
+            }
+            ans=Math.max(ans,right-left+1);
+        }
+        return ans;
+
+        //------------------Another solution---------------------
+        /*int left=0;
         int right;
         for(right=0;right< nums.length;right++){
             if(nums[right]==0){
@@ -13,7 +31,7 @@ public class MaxConsecutiveOnesIII {
                 left++;
             }
         }
-        return right-left;
+        return right-left;*/
     }
     public static void main(String[] args){
         MaxConsecutiveOnesIII m=new MaxConsecutiveOnesIII();
