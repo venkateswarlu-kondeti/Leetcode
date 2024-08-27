77a74b1ef7ed799ef17c4e0fac96d2fe13f20a01
venkateswarlu-kondeti <venchitin@gmail.com>
2024-08-27 16:55:10 -0500

    Today First Commit 08/27/2024

--- src/com/stacksandqueues/BackspaceStringCompare.java
+++ src/com/stacksandqueues/BackspaceStringCompare.java
@@ -1,7 +1,30 @@
 package com.stacksandqueues;
 
+import java.util.Stack;
+
 public class BackspaceStringCompare {
     public static boolean backspaceCompare(String s, String t){
+        return build(s).equals(build(t));
+    }
+    public static String build(String str){
+        Stack<Character> stack=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(Character c : str.toCharArray()){
+            if(c!='#'){
+                stack.push(c);
+            }else if(!stack.empty()){
+                stack.pop();
+            }
+        }
+        while(!stack.empty()){
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+
+    //----------------------Another Solution---------------------------------
+
+    /*public static boolean backspaceCompare(String s, String t){
         return build(s).equals(build(t));
     }
     public static String build(String str){
@@ -14,10 +37,13 @@ public class BackspaceStringCompare {
             }
         }
         return stack.toString();
-    }
+    }*/
     public static void main(String[] args){
-        String s="ab#c";
+        /*String s="ab#c";
         String t="ad#c";
+        System.out.println("The string after comparing backspace string is: "+backspaceCompare(s,t));*/
+        String s="a##c";
+        String t="#a#c";
         System.out.println("The string after comparing backspace string is: "+backspaceCompare(s,t));
     }
 }
--- src/com/stacksandqueues/DailyTemperatures.java
+++ src/com/stacksandqueues/DailyTemperatures.java
@@ -17,7 +17,8 @@ public class DailyTemperatures {
         return answer;
     }
     public static void main(String[] args){
-        int[] temp={40,35,32,37,50};
-        System.out.println("The days between warmer and colder temperatures are: "+ Arrays.toString(dailyTemperatures(temp)));
+        //int[] temp={40,35,32,37,50};
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        System.out.println("The days between warmer and colder temperatures are: "+ Arrays.toString(dailyTemperatures(temperatures)));
     }
 }
