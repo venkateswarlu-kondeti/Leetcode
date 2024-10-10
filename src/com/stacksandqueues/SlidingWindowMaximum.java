2a49356505a07faca31442ac69929c2c9944b991
venkateswarlu-kondeti <venchitin@gmail.com>
2024-10-10 17:18:14 -0500

    Today First Commit 10/10/2024

--- src/com/stacksandqueues/SlidingWindowMaximum.java
+++ src/com/stacksandqueues/SlidingWindowMaximum.java
@@ -1,30 +1,38 @@
 package com.stacksandqueues;
-
-import java.util.ArrayDeque;
-import java.util.Arrays;
-
+import java.util.*;
 public class SlidingWindowMaximum {
     public static int[] maxSlidingWindow(int[] nums, int k){
-        int[] ans=new int[nums.length-3+1];
-        ArrayDeque<Integer> queue=new ArrayDeque<>();
-        for(int i=0;i< nums.length;i++){
-            if(!queue.isEmpty() && nums[i]>nums[queue.getLast()]){
-                queue.removeLast();
+        Deque<Integer> deq=new ArrayDeque<>();
+        List<Integer> result=new ArrayList<>();
+        for(int i=0;i<k;i++){
+            while(!deq.isEmpty() && nums[i]>= nums[deq.peekLast()]){
+                deq.removeLast();
             }
-            queue.addLast(i);
-
-            if(queue.getFirst()+3==i){
-                queue.removeFirst();
-            }
-
-            if(i>=k-1){
-                ans[i-k+1]=nums[queue.getFirst()];
-            }
+            deq.addLast(i);
         }
-        return ans;
+        result.add(nums[deq.peekFirst()]);
+        for(int i=k;i< nums.length;i++){
+            if(deq.peekFirst()==i-k){
+                deq.removeFirst();
+            }
+            while(!deq.isEmpty() && nums[i]>= nums[deq.peekLast()]){
+                deq.removeLast();
+            }
+            deq.addLast(i);
+            result.add(nums[deq.peekFirst()]);
+        }
+        return result.stream().mapToInt(i->i).toArray();
     }
     public static void main(String[] args){
-        int[] nums={1,3,-1,-3,-2,3,6,7};
+        /*int[] nums={1,3,-1,-3,-2,3,6,7};
+        int k=3;*/
+        /*int[] nums={1,3,-1,-3,5,3,6,7};
+        int k=3;*/
+        /*int[] nums={1};
+        int k=1;*/
+        /*int[] nums={7,2,4};
+        int k=2;*/
+        int[] nums={1,3,1,2,0,5};
         int k=3;
         System.out.println("The maximum of sliding window is: "+ Arrays.toString(maxSlidingWindow(nums,k)));
     }
