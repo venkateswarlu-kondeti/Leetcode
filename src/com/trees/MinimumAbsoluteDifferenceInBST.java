c073c592a79f41eecbdad468d1cdee0408f3836d
venkateswarlu-kondeti <venchitin@gmail.com>
2025-03-16 19:34:24 -0500

    Today First Commit 03/16/2025

--- src/com/trees/MinimumAbsoluteDifferenceInBST.java
+++ src/com/trees/MinimumAbsoluteDifferenceInBST.java
@@ -1,28 +1,31 @@
 package com.trees;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MinimumAbsoluteDifferenceInBST {
+    public static List<Integer> valList;
     public static int getMinimumDifference(TreeNode root){
-        List<Integer> values=new ArrayList<>();
-        dfs(root,values);
+        valList=new ArrayList<>();
+        dfs(root);
+        Collections.sort(valList);
         int ans=Integer.MAX_VALUE;
-        for(int i=1;i<values.size();i++){
-            ans=Math.min(ans,Math.abs(values.get(i)-values.get(i-1)));
+        for(int i=1;i<valList.size();i++){
+            ans=Math.min(ans,Math.abs(valList.get(i)-valList.get(i-1)));
         }
         return ans;
     }
-    public static void dfs(TreeNode node, List<Integer> values){
+    public static void dfs(TreeNode node){
         if(node==null){
             return;
         }
-        dfs(node.left,values);
-        values.add(node.val);
-        dfs(node.right,values);
+        valList.add(node.val);
+        dfs(node.left);
+        dfs(node.right);
     }
     public static void main(String[] args){
-        TreeNode root=new TreeNode(9);
+        /*TreeNode root=new TreeNode(9);
         TreeNode five=new TreeNode(5);
         TreeNode one=new TreeNode(1);
         TreeNode seven=new TreeNode(7);
@@ -30,7 +33,17 @@ public class MinimumAbsoluteDifferenceInBST {
         root.left=five;
         five.left=one;
         five.right=seven;
-        root.right=fifteen;
+        root.right=fifteen;*/
+
+        TreeNode root=new TreeNode(4);
+        TreeNode two=new TreeNode(2);
+        TreeNode one=new TreeNode(1);
+        TreeNode three=new TreeNode(3);
+        TreeNode six =new TreeNode(6);
+        root.left=two;
+        two.left=one;
+        two.right=three;
+        root.right= six;
         System.out.println("The minimum absolute difference of BST is: "+MinimumAbsoluteDifferenceInBST.getMinimumDifference(root));
     }
     public static class TreeNode{
