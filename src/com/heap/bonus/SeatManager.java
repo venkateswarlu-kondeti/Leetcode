d723e5365e2cff1cee1e74104e2e1ac2746f957c
venkateswarlu-kondeti <venchitin@gmail.com>
2025-12-18 21:36:50 -0600

    Commit 12/18

---
+++ src/com/heap/bonus/SeatManager.java
@@ -0,0 +1,32 @@
+package com.heap.bonus;
+
+import java.util.PriorityQueue;
+
+public class SeatManager {
+    public PriorityQueue<Integer> heap;
+    public SeatManager(int n) {
+        heap=new PriorityQueue<>();
+        for(int i=1;i<=n;i++){
+            heap.add(i);
+        }
+    }
+
+    public int reserve() {
+        return heap.remove();
+    }
+
+    public void unreserve(int seatNumber) {
+        heap.add(seatNumber);
+    }
+    public static void main(String[] args){
+        SeatManager seatManager=new SeatManager(5);
+        System.out.println(seatManager.reserve());
+        System.out.println(seatManager.reserve());
+        seatManager.unreserve(2);
+        System.out.println(seatManager.reserve());
+        System.out.println(seatManager.reserve());
+        System.out.println(seatManager.reserve());
+        System.out.println(seatManager.reserve());
+        seatManager.unreserve(5);
+    }
+}
