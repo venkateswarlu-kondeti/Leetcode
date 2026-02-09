44e7ffc9616f6a6362c2ed885b9f8fa1f1ee7130
venkateswarlu-kondeti <venchitin@gmail.com>
2026-02-09 17:56:21 -0600

    Commit 02/09

--- src/com/greedy/bonus_greedy/VideoStitching.java
+++ src/com/greedy/bonus_greedy/VideoStitching.java
@@ -29,52 +29,6 @@ public class VideoStitching {
             }
         }
         return -1;
-
-
-
-
-
-
-
-
-        /*PriorityQueue<int[]> minHeap=new PriorityQueue<>((a,b) ->{
-            if(a[0]==b[0]){
-                return b[1]-a[1];
-            }
-            return a[0]-b[0];
-        });
-        for(int[] clip : clips){
-            minHeap.add(new int[]{clip[0],clip[1]});
-        }
-        List<int[]> filteredClips=new ArrayList<>();
-        int[] limit={0,time};
-        int min=minHeap.peek()[0], max=minHeap.peek()[1];
-        while(!minHeap.isEmpty()){
-            int[] curr= minHeap.remove();
-            if(curr[1]>limit[0]){
-                limit[0]=curr[1];
-                filteredClips.add(curr);
-            }
-        }
-        for(int[] clip : filteredClips){
-            System.out.println(Arrays.toString(clip));
-        }
-        Set<Integer> finalClips=new HashSet<>();
-        finalClips.add(min);
-        for(int i=1;i<filteredClips.size();i++){
-            if(filteredClips.get(i)[0]<=max){
-                finalClips.add(max);
-            }else if(filteredClips.get(i)[0]<=filteredClips.get(i-1)[1]){
-                finalClips.add(filteredClips.get(i)[0]);
-            }else{
-                return -1;
-            }
-
-            if(filteredClips.get(i)[1]>=time){
-                return finalClips.size();
-            }
-        }
-        return -1;*/
     }
     public static void main(String[] args){
         //int[][] clips = {{0,2},{4,6},{8,10},{1,9},{1,5},{5,9}}; int time = 10;
